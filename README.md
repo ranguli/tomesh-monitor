f91df7b2484281ad2a510f70914cd935f3693a20
ranguli <hello@joshmurphy.ca>
2019-06-15 20:06:54 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# tomesh monitor
+
+A small flask webapp designed to be run in Chromium kiosk mode on the LCD of a Raspberry Pi Tomesh node
+
+![screenshot](screenshot.png)
---
+++ monitor.py
@@ -0,0 +1,32 @@
+import netifaces as ni
+import subprocess
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+class Adapter():
+
+   def __init__(self, name, ip, status):
+      self.ip = ip
+      self.name = name
+      self.status = status 
+
+@app.route('/')
+def index():
+  
+   # Get all *physical* network adapters
+   adapters = subprocess.getoutput("find /sys/class/net -type l -not -lname '*virtual*' -printf '%f\n'").split()
+   result = []
+   
+   for name in adapters:
+      ip = ni.ifaddresses(name)[ni.AF_INET][0]['addr']
+      status = subprocess.getoutput("cat /sys/class/net/" + name + "/operstate")
+      adapter = Adapter(name, ip, status)
+      print(name)
+      result.append(adapter)
+
+   return render_template('index.html', adapters=result)
+
+if __name__ == '__main__':
+   app.run(debug = False)
